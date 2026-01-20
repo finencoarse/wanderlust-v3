a7fc14f32ccdf13991ee1a82e53616243d32d831
finencoarse <154966371+finencoarse@users.noreply.github.com>
2026-01-20 16:01:05 +0800

    Update vite-env.d.ts

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,3 +1,15 @@
+
+/// <reference types="vite/client" />
+
+interface Window {
+  gapi: any;
+  google: any;
+  aistudio?: {
+    hasSelectedApiKey: () => Promise<boolean>;
+    openSelectKey: () => Promise<void>;
+  };
+}
+
 declare namespace NodeJS {
   interface ProcessEnv {
     API_KEY: string;
